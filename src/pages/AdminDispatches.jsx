5063e36e2e42a5ec24cefc17666d7b0db27cd1d4
Capital Contracting Group <capitalcontractinggroupnj@gmail.com>
2026-02-28 05:08:45 +0000

    File changes

--- src/pages/AdminDispatches.jsx
+++ src/pages/AdminDispatches.jsx
@@ -378,7 +378,7 @@ export default function AdminDispatches() {
                   </div>
                 </div>
               </CardContent>
-            </Card>
+              </Card>
             </div>
           ))}
         </div>
